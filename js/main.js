843bb9191b55751c027ad9b15cf4e8598cbafb7d
Victor00255 <152938199+Victor00255@users.noreply.github.com>
2024-12-17 21:57:02 -0500

    Whatsapp verification number

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-// Add all your JS here
+{
+   "phone_number": "YOUR_NUMBER",
+   "portable": true,
+   "url": "https://numbers.twilio.com/v1/Porting/Portability/PhoneNumber/YOUR_NUMBER",
+   "country": "US",
+   "not_portable_reason": null,
+   "account_sid": null,
+   "pin_and_account_number_required": false,
+   "not_portable_reason_code": null,
+   "number_type": "LOCAL"
+}
